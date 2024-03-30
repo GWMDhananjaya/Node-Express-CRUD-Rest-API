1cbc81c7e7b372bd6d31f6aca6c4815d72d654fb
DHANANJAYA RUPASINGHE <106276257+Dhananjaya0216@users.noreply.github.com>
2024-03-30 17:51:32 +0530

    Fix server.js file to include database connection and API endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,22 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'school'
+});
+
+app.length('/', (req, res) => {
+    
+    const sql = "SELECT * FROM student";
+    db.query(sql, (err, result) => {
+        if(err) return res.json({Message:"Error inside server"});
+        return res.json(result);
+    });
+});
 app.listen(8081, () => {
-  console.log('Serveris running on port 8081');
+  console.log('Server is running on port 8081');
+
 });
