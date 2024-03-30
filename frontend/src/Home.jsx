2f933aa9d1a059e752f3cf983b15cea55d33d606
DHANANJAYA RUPASINGHE <106276257+Dhananjaya0216@users.noreply.github.com>
2024-03-30 18:31:00 +0530

    Add table with student data and action buttons

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -11,8 +11,34 @@ export default function Home() {
 
 
     return (
-    <>
-    
-    </>
+    <div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((student, index) =>{
+                        return (
+                            <tr key={index}>
+                                <td>{student.ID}</td>
+                                <td>{student.Name}</td>
+                                <td>{student.Email}</td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    </div>
   )
 }
